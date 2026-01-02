c67296deac9b9813e5a655d74d8eb6be4090bc96
Dev-YoungPandas <rajpardhi962000@gmail.com>
2026-01-02 19:05:21 +0530

    code

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      {/* <GridAnimation />
-      <Whatwedo />
+      <GridAnimation />
+      {/* <Whatwedo />
       <OurMethod />
       <Whoweare /> */}
     </div>
