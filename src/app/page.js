b4b08c4383e0813974f49d0d5680999f37afe678
Dev-YoungPandas <rajpardhi962000@gmail.com>
2026-01-02 19:08:52 +0530

    hgjfc

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
       <Hero />
       {/* <GridAnimation /> */}
       <Whatwedo />
-      {/* <OurMethod />
-      <Whoweare /> */}
+      <OurMethod />
+      <Whoweare />
     </div>
   );
 }
